eae5d7edea64716b03fcd50fecd0727386a7203b
dcDalin <mcdalinoluoch@gmail.com>
2019-09-27 14:43:43 +0300

    Setup routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from '../components/Footer';
-import ResponsiveContainer from '../components/ResponsiveContainer';
+import ResponsiveContainer from '../components/ResponsiveContainer/ResponsiveContainer';
+import Content from '../components/Content';
 
 const App = () => (
-  <ResponsiveContainer>
-    <Footer />
-  </ResponsiveContainer>
+  <Router>
+    <ResponsiveContainer>
+      <Content />
+      <Footer />
+    </ResponsiveContainer>
+  </Router>
 );
 
 export default App;
--- src/containers/desktopContainer.js
+++ src/containers/desktopContainer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Responsive, Menu, Input, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import getWidth from '../utils/getWidth';
 import WibLogo from '../assets/wib-logo.png';
 
-const DesktopContainer = ({ children }) => {
+const DesktopContainer = ({ children, activeItem, onClick }) => {
   return (
     <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
       <Menu borderless>
-        <Menu.Item>
+        <Menu.Item name="home" active={activeItem === 'home'} onClick={onClick} as={Link} to="/">
           <Image src={WibLogo} size="mini" verticalAlign="top" />
         </Menu.Item>
         <Menu.Item name="messages" />
-        <Menu.Item name="friends" />
+        <Menu.Item name="faq" active={activeItem === 'faq'} onClick={onClick} as={Link} to="/faq" />
         <Menu.Menu position="right">
           <Menu.Item>
             <Input icon="search" placeholder="Search..." />
