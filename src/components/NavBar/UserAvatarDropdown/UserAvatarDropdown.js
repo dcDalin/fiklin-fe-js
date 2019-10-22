b902cae6b7af8affeeaba2f3b4525867ba82ca6d
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2019-10-22 22:21:47 +0300

    Profile page ui draft

--- src/components/NavBar/UserAvatarDropdown/UserAvatarDropdown.js
+++ src/components/NavBar/UserAvatarDropdown/UserAvatarDropdown.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import DropDownTrigger from './DropDownTrigger';
 import './UserAvatarDropdown.css';
 
@@ -14,7 +15,7 @@ const UserAvatarDropdown = ({ logout, user }) => (
         </span>
       </Dropdown.Item>
       <Dropdown.Divider className="custom-divider" />
-      <Dropdown.Item text="My Profile" />
+      <Dropdown.Item text="My Profile" as={Link} to="/profile" />
       <Dropdown.Item text="Save as" />
       <Dropdown.Item text="Log Out" onClick={logout} />
     </Dropdown.Menu>
